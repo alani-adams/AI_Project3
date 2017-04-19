9b7b3c101708b5585279d33b46ac38a92020d214
alp13d <alp13d@acu.edu>
2017-04-19 00:28:32 -0500

    Add files via upload

--- learning.cpp
+++ learning.cpp
@@ -19,9 +19,9 @@ int ifWin(string board, state s)
 {
 	//continue == 0, winner player1 == 1, winner player2 == 2, draw == 3
 	if((board[0] == board[1] == board[2]) && (board[0] == 'X') )
-		return learningFactor(1,2,s);
+		return 1;
 	else if((board[0] == board[1] == board[2]) && (board[0] == 'O') )
-		return learningFactor(2,1,s);
+		return 2;
 	else if((board[3] == board[4] == board[5]) && (board[3] == 'X') )
 		return learningFactor(1,2,s);
 	else if((board[3] == board[4] == board[5]) && (board[3] == 'O') )
@@ -80,7 +80,7 @@ state possibleStates( state s, string board, int player )
 		{
 			action act;
 			act.location = j;
-			act.value = 0.5;//IDK WHAT THE VALUE IS!!!!!!!!!!!!!!
+			act.value = 0.5;//IDK WHAT THE VALUE IS!!!!!!!!! Do we need to look it up somehow?
 			if(player == 1)
 			{
 				temp[j] = 'X';
@@ -96,28 +96,45 @@ state possibleStates( state s, string board, int player )
 	}
 }
 
-newState()
+void evaluate() // finds the best action to take
 {
+	if(ifWin(board, s) == 1)
+		learningFactor(1,2,player1State, player2State);
+	else if(ifWin(board, s) == 2)
+		learningFactor(2,1,player2State,player1State);
+	else if(ifWin(board, s) == 3)//out of moves/draw
+		return;//end the game
+	else
+		;//continue the game, dependent on main cpp file
 
 }
 
-evaluate()
+chooseAction() 
 {
+	//choose randomly taking values in to consideration, 
+	//favor exploration (choosing lower values disproportionately), 
+	//favor exploitation (choosing higher values disproportionately).
+
 
 }
 
-chooseState()
-{
-
-}
-
-int learningFactor(int winner, int loser, state s)
+void learningFactor(int winner, int loser, state finalStateWinner, state finalStateLoser) //applies learning feature
 {
 	//apply 1 to last move
+	finalStateWinner.actions[finalStateWinner.actions.size()].value = 1;
+	float decay = 1;
+	for(int i=1; i<finalStateWinner.actions.size(); i++)
+	{
+		finalStateWinner.actions[i].value += (finalStateWinner.actions[i].value * (0.1 * decay));
+		decay *= 0.2;
+	}
 
-
-
-	return winner;
+	decay = 1;
+	for(int i=1; i<finalStateLoser.actions.size(); i++)
+	{
+		finalStateLoser.actions[i].value -= (finalStateLoser.actions[i].value * (0.1 * decay));
+		decay *= 0.2;
+	}
 }
 
 
